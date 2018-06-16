2ade5782dce972150df75645b2daf3f2e70551bc
Joe Thor <joethor1989@gmail.com>
2018-06-16 17:16:25 -0500

    began programming RPS without different user sessions

--- RPS-Multiplayer/assets/javascript/app.js
+++ RPS-Multiplayer/assets/javascript/app.js
@@ -11,53 +11,56 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  //Creates a database variable which is linked to firebase
-  var database = firebase.database();
-  var initialValueClicks = 0;
+//Creates a database variable which is linked to firebase
+var database = firebase.database();
+
+var choicesArr = ["rock", "paper", "scissors"]
+
+var activePlayer = 1
  
-  //This is something I will probably use 
-  database.ref().on("value", function(snapshot) {
-    
-   if(!snapshot.val().clickCount){
-     clicks = 0;
-     console.log("clicks: " + clicks)
-     database.ref().set({
-      testValue: 0,
-      testPlayer: "MonsterMash",
-      clickCount: clicks
-     })
-   }
+renderChoices()
 
-    clicks = snapshot.val().clickCount;
-    console.log("Clicks after snapshot: "+clicks)
-    $("#clicks").html(clicks)
-    $("#testName").html(snapshot.val().testPlayer)
-    $("#testNumber").html(snapshot.val().testValue)
-  
-    clicks = snapshot.val().clickCount;
-  console.log(snapshot)
-    })
+$('body').on('click', '.choice ', function () {
 
-$("#button").on("click", function() {
- 
-  // Reduce the clickCounter by 1
-  clicks ++
- console.log(clicks)
- //so, if I am SETTING data...it seems it overwrites the database everytime. Which means it if I JUST pass clickCount: clicks I lose all testValue and testPlayer data...
- //Perhaps this is where child attributes come into play..though it MAY not look that way, depending on how jquery works.
- //This has HUGE ramifcations...creating a server side OBJECT to interact with is probably key
-  database.ref().set({
-    clickCount: clicks,
-    testValue: 0,
-    testPlayer: "MonsterMash"
-  });
-  // Alert User and reset the counter
-  // if (clicks === 0) {
+ var whichClicked = $(this).attr("data-type");
+ database.ref().push({
+  choice: whichClicked,
+  playerNumber: activePlayer
+});
 
-  //   alert("Phew! You made it! That sure was a lot of clicking.");
+if(activePlayer === 1) {
+  activePlayer = 2
+}
+else{
+ activePlayer = 1
 
-    
+}
+renderChoices()
 
-  // }
+ console.log(whichClicked);
 })
 
+
+function renderChoices() {
+  for (i = 0; i < choicesArr.length; i++) {
+    var newP = $("<p>");
+    newP.attr("data-type", choicesArr[i]);
+    newP.addClass("choice");
+    newP.html(choicesArr[i]);
+    
+    console.log("Done@!");
+    if(activePlayer === 1) {
+      $("#player1").append(newP)
+      $("#player2").empty()
+    }
+    else{
+      $("#player2").append(newP)
+      $("#player1").empty()
+    }
+  
+  }
+}
+
+function decideWinner (choice1, choice2) {
+  
+}
