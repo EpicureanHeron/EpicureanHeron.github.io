8bcbe6baaf57c94636d3faef8202e9f3d8d28466
Joe Thor <joethor1989@gmail.com>
2018-07-20 07:19:24 -0500

    inquirer structured for entering non numbers and flows about right, check to see if game wants to be played again

--- constructor-word-guess/index.js
+++ constructor-word-guess/index.js
@@ -1,7 +1,69 @@
+// **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
+
+// * Randomly selects a word and uses the `Word` constructor to store it
+        //an array of words which gets passed to word.js 
+
+// * Prompts the user for each guess and keeps track of the user's remaining guesses
+        //inquirer 
+
+// 3. `Letter.js` *should not* `require` any other files.
+
+// 4. `Word.js` *should only* require `Letter.js`
+
+
+
+
 var inquirer = require("inquirer")
 var wordConst = require("./word.js")
 
 var wordImport = new wordConst()
 
 
-wordImport.wordValue()
+var count = 0;
+
+playGame()
+
+function playGame() {
+    //randomly choses a word
+    //pass word to word constructor
+    //guesses remaining ?
+    if (count < 5) {
+        inquirer.prompt([
+            {
+                name: "guess",
+                message: "Guess a letter: ",
+                validate: function (value) {
+                    //logic below will need to be updated to check if the letter has already been guessed
+                    if (isNaN(value) === true) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function (answers) {
+            count ++ 
+            console.log(answers.guess)
+            playGame()
+        });
+        //wordImport.wordValue()
+
+    }
+    else{
+        console.log("you lost!")
+        inquirer.prompt([
+            
+                {
+                    type:"confirm",
+                    name: "playAgain",
+                    message:"Play again?"
+                }
+        ]).then(function (answers) {
+            if(answers.playAgain === true){
+                count = 0
+               
+                playGame()
+            }
+        });
+        //wordImport.wordValue()
+    }
+}
--- constructor-word-guess/letter.js
+++ constructor-word-guess/letter.js
@@ -1,3 +1,16 @@
+// Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+
+// * A string value to store the underlying character for the letter
+
+// * A boolean value that stores whether that letter has been guessed yet
+
+// * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+
+// * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+
+
+
 var Letter = function(){
     this.checkLetter = function(someLetter) {
         console.log("check letter responds with the input: " + someLetter)
