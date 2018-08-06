f0a1d129471140c6ed39afd779d2e0a4cc47ef6b
Joe Thor <joethor1989@gmail.com>
2018-08-06 18:27:00 -0500

    refactored code from survey.html's javascript to work in apiroutes.js. Added readme.md

--- friendFinder/app/routing/apiRoutes.js
+++ friendFinder/app/routing/apiRoutes.js
@@ -3,13 +3,41 @@
 
 var friends = require("../data/friends.js")
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res){
+    app.get("/api/friends", function (req, res) {
         res.json(friends)
-        })
-    app.post("/api/friends", function(req, res) {
+    })
+
+    app.post("/api/friends", function (req, res) {
+
+        var scoresIntArr = req.body.scores
+
+        var matchedTotal = 100;
+        var matchedIndex;
+
+        //For loop cycles through every friend in the data array, but excludes the final array item because that will be the most recently posted friend
+        for (j = 0; j < friends.length; j++) {
+
+            //variable where the difference between scores is kept
+            var questionDifference = 0;
+
+            //cycles through the potential friend's scores
+            for (i = 0; i < friends[j].scores.length; i++) {
+                //adds the absolute value of the the difference beween the poster's score and the potential friends
+                questionDifference += Math.abs(parseInt(scoresIntArr[i]) - parseInt(friends[j].scores[i]))
+
+            }
+
+            //checks to see if the new value is less than the current value 
+            if (questionDifference < matchedTotal) {
+                matchedTotal = questionDifference
+                matchedIndex = j
+            }
+            
+        }
         friends.push(req.body);
-        res.jsonp(friends);
+        res.jsonp(friends[matchedIndex]);
     })
 }
+
